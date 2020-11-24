e4db2d36452c9c032d706d80e874a2a8ffeb49df
TheRealNeathed <64224708+TheRealNeathed@users.noreply.github.com>
2020-11-24 10:11:09 -0500

    Add files via upload

---
+++ C74-master/Screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text,View,TouchableOpacity,StyleSheet,ScrollView,FlatList} from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import db from '../config'
+
+
+export default class LoginScreen extends React.Component{
+    constructor(){
+    super();
+    }
+    
+    render(){
+        return(
+        
+     )
+    }
+      
+
+        
+}
